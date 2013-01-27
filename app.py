ed50bd698209ca31f15b47a4681ba49a37d9451d
Jon Wong <j@jnwng.com>
2013-01-26 22:32:08 -0800

    Structural brunch commit

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import os
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
+import jinja2
+
+app = Flask(__name__, static_folder="public", static_url_path="")
+jinja_loader = jinja2.ChoiceLoader([
+    app.jinja_loader,
+    jinja2.FileSystemLoader(os.path.dirname(os.path.abspath(__file__)) + 
+    '/public'),
+])
+app.jinja_loader = jinja_loader
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
