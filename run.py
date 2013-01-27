0821eea3dcd198c8d4ba908d4583f7cd9ff49321
Jon Wong <j@jnwng.com>
2013-01-26 23:52:45 -0800

    Fixing run.py

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
+import os
+
 from flask import Flask
 from main.views import main
 
 app = Flask(__name__)
+app.config.from_object('config')
 app.register_blueprint(main)
 
-app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    debug = port == 5000
+    app.run(host='0.0.0.0', port=port, debug=debug)
