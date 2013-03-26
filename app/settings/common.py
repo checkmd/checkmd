28e94312836ebb8d140201d5143a58717d61f40e
Jon Wong <j@jnwng.com>
2013-03-25 19:13:25 -0700

    Removing production config from common

--- app/settings/common.py
+++ app/settings/common.py
@@ -201,11 +201,3 @@ LOGGING = {
         },
     }
 }
-
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-if os.getenv('DATABASE_URL'):
-    DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
